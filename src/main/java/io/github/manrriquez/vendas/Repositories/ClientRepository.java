137a1769a4c2f09a8112610b17e75421aca86708
Manrriquez <manrriquez.contato@gmail.com>
2022-12-15 20:11:53 -0300

    modulo basico

---
+++ src/main/java/io/github/manrriquez/vendas/Repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package io.github.manrriquez.vendas.Repositories;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository {
+}
---
+++ src/main/java/io/github/manrriquez/vendas/models/ClientModel.java
@@ -0,0 +1,4 @@
+package io.github.manrriquez.vendas.models;
+
+public class ClientModel {
+}
